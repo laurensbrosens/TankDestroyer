a829e900ff1188146aed388630c0d07e82eb7d53
Hendrik <hendrikdejonge@hotmail.com>
2026-04-21 15:28:43 +0200

    Intial setup

---
+++ Source/TankDestroyer.Engine/GameSnapshot.cs
@@ -0,0 +1,9 @@
+﻿namespace TankDestroyer.Engine;
+
+public class GameSnapshot
+{
+    public int Turn { get; set; } = 0;
+    public World World { get; set; }
+    public Tank[] Tanks { get; set; }
+    public PlayerBot Players { get; set; }
+}
